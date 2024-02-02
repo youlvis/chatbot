0d05eafe0d867b509ec797a1d4da7e2c58085d95
Youlvis Jose Jimenez Peñate <yojimene@bancolombia.com.co>
2024-02-02 09:39:37 -0500

    chat

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
   private conversation: { type: string; message: string }[] = [];
+  // private apiUrl = 'https://api.openai.com/v1/completions'; 
+  private apiUrl = 'https://api.quotable.io/random'; 
+
+  private apiKey = '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';
+
+  constructor(private http: HttpClient) {}
 
   addMessage(type: string, message: string): void {
     this.conversation.push({ type, message });
@@ -17,5 +25,24 @@ export class ChatService {
 
   getConversation(): { type: string; message: string }[] {
     return this.conversation;
+  }
+
+  // sendMessageToChatGPT(userMessage: string): Observable<any> {
+  //   const headers = new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer ${this.apiKey}`,
+  //   });
+
+  //   const requestData = {
+  //     prompt: userMessage,
+  //     max_tokens: 100,
+  //     model: "text-similarity-davinci-001"
+  //   };
+
+  //   return this.http.post<any>(this.apiUrl, requestData, { headers });
+  // }
+
+  sendMessage(userMessage: string): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
   }
 }
--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -9,15 +9,22 @@ import { ChatService } from '../chat.service';
 export class ChatbotComponent {
   userInput = '';
 
-  constructor(public chatService: ChatService) {}
+  constructor(public chatService: ChatService) { }
 
   sendMessage(): void {
     const userMessage = this.userInput.trim();
-    if (userMessage !== '') {
-      this.chatService.addMessage('user', userMessage);
-      this.chatService.addMessage('chatbot', '¡Hola! Soy un chatbot. ¿En qué puedo ayudarte?');
-      this.userInput = ''; 
-    }
+    this.chatService.sendMessage("").subscribe(
+      (response: any) => {
+        const chatbotMessage = response.content || 'No se pudo obtener una cita aleatoria.';
+        this.chatService.addMessage('user', userMessage);
+        this.chatService.addMessage('chatbot', chatbotMessage);
+      },
+      (error: any) => {
+        console.error('Error al llamar a la API de Random Quotes:', error);
+      }
+    );
+
+    this.userInput = '';
   }
 
   clearConversation(): void {
